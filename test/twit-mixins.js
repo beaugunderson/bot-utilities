03d1fc33e47a2de97e815d271aca92014246ba6a
Beau Gunderson <beau@beaugunderson.com>
2015-06-05 11:07:49 -0700

    Update packages, add buffer test

--- test/twit-mixins.js
+++ test/twit-mixins.js
@@ -13,7 +13,25 @@ var _ = require('lodash');
 _.mixin(Twit.prototype, botUtilities.twitMixins);
 
 describe('updateWithMedia', function () {
-  it('should update with media', function (cb) {
+  it('should update with media from a buffer', function (cb) {
+    var twit = new Twit(botUtilities.getTwitterAuthFromEnv());
+
+    var buffer = fs.readFileSync(path.resolve(__dirname, 'kitten.jpg'));
+
+    twit.updateWithMedia({status: 'Just testing...'}, buffer,
+        function (err, data, result) {
+      should.not.exist(err);
+      should.not.exist(data.errors);
+
+      result.statusCode.should.equal(200);
+
+      data.extended_entities.media.should.have.length(1);
+
+      cb();
+    });
+  });
+
+  it('should update with media from a stream', function (cb) {
     var twit = new Twit(botUtilities.getTwitterAuthFromEnv());
 
     var stream = fs.createReadStream(path.resolve(__dirname, 'kitten.jpg'));
@@ -21,6 +39,7 @@ describe('updateWithMedia', function () {
     twit.updateWithMedia({status: 'Just testing...'}, stream,
         function (err, data, result) {
       should.not.exist(err);
+      should.not.exist(data.errors);
 
       result.statusCode.should.equal(200);
 
